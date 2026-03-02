7a41f16df67ad400ef014775a06a97e9c7c6f43d
Fares Chehidi <fareschehidi7@gmail.com>
2026-03-02 16:11:40 +0100

    feat: app-test blink LED PA5 bare-metal

--- app-test/src/main.rs
+++ app-test/src/main.rs
@@ -4,7 +4,27 @@
 use cortex_m_rt::entry;
 use panic_halt as _;
 
+const RCC_AHB1ENR: *mut u32 = 0x4002_3830 as *mut u32;
+const GPIOA_MODER: *mut u32 = 0x4002_0000 as *mut u32;
+const GPIOA_ODR: *mut u32 = 0x4002_0014 as *mut u32;
+
 #[entry]
 fn main() -> ! {
-    loop {}
+    unsafe {
+        // Init une seule fois
+        let rcc = core::ptr::read_volatile(RCC_AHB1ENR);
+        core::ptr::write_volatile(RCC_AHB1ENR, rcc | 1);
+        let moder = core::ptr::read_volatile(GPIOA_MODER);
+        core::ptr::write_volatile(GPIOA_MODER, (moder & !(0b11 << 10)) | (0b01 << 10));
+    }
+
+    loop {
+        unsafe {
+            let odr = core::ptr::read_volatile(GPIOA_ODR);
+            core::ptr::write_volatile(GPIOA_ODR, odr ^ (1 << 5));
+        }
+        for _ in 0..1_000_000 {
+            cortex_m::asm::nop();
+        }
+    }
 }
